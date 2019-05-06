6233b93a51f45b8688371fbc8bfaf3ce905eb648
Thierry Joux <tjoux@hotmail.com>
2019-05-06 13:07:17 -0700

    MVP requirements completed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,7 +11,9 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    console.log("MOVIE MOUNTED")
+    const id = this.props.match.params.id;
+    
     this.fetchMovie(id);
   }
 
